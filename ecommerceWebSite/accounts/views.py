9af96574d7ab0d67f0bd7e6e0f47822d971f07aa
omar elfarsaoui <adilfarsaoui7@gmail.com>
2020-05-22 22:37:56 +0000

    * add login / signup function * add login / signup view (not finished )

---
+++ ecommerceWebSite/accounts/views.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+
+from weShop.models import OrderItem
+
+urlsAllowtoRedirect = []
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'account/signup.html', {'form': form})
+
+
+
+def login_view(request):
+    # Todo = add validation to secure redirect
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            request.session['cart'] = getItemIncart(user)
+            return redirect('shop')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+
+def getItemIncart(user):
+    return len(OrderItem.objects.filter(user=user))
